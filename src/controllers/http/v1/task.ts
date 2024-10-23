5ed790cd96da94dc459cee3fdc9c74ee35416d46
Мухин Григорий <muhin.g.s.00@gmail.com>
2024-10-23 20:46:37 +0300

    refactoring task controller

--- src/controllers/http/v1/task.ts
+++ src/controllers/http/v1/task.ts
@@ -1,7 +1,8 @@
-import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from 'fastify';
 import { UseCaseTask } from '@/domain/use-cases/task';
 import { TaskEntity } from '@/domain/entities/task';
 import { AuthMiddleware, userId } from '../middleware/auth';
+import { baseHttpResponseMapping } from '../response/error';
 
 export const prefixTask = '/task';
 
@@ -9,102 +10,195 @@ export class TaskHandler {
 	constructor(private useCaseTask: UseCaseTask, private authMiddleware: AuthMiddleware) {
 	}
 
+	private getTaskHandler = async (req: FastifyRequest, reply: FastifyReply) => {
+		const task = await this.useCaseTask.getTask((req.params as {uuid: string})['uuid'], req[userId]);
+
+		const response = {
+			msg: 'get task',
+			...task,
+		}
+		return reply.code(200).send(response);
+	}
+
+	private patchTaskHandler = async (req: FastifyRequest, reply: FastifyReply) => {
+		const requestData = JSON.parse(req.body as string) as TaskEntity;
+
+		if(!requestData.description && !requestData.title && !requestData.isCompleted) {
+			return reply.code(401).send({ error: 'not valid' })
+		}
+
+		const task = await this.useCaseTask.updateTask(requestData, req[userId]);
+
+		const response = {
+			msg: 'update',
+			...task,
+		}
+
+		return reply.code(200).send(response);
+	}
+
+	private deleteTaskHandler = async (req: FastifyRequest, reply: FastifyReply) => {
+		await this.useCaseTask.deleteTask((req.params as {uuid: string})['uuid'], req[userId]);
+
+		return reply.code(200).send({status: 'ok'});
+	}
+
+	private createTaskHandler = async (req: FastifyRequest, reply: FastifyReply) => {
+		const requestData = JSON.parse(req.body as string) as TaskEntity;
+
+		if(!requestData.description || !requestData.title || !requestData.isCompleted) {
+			return reply.code(401).send({ error: 'not valid' })
+		}
+
+		const newTask: TaskEntity = {
+			userId: req[userId],
+			title: requestData.title,
+			isCompleted: requestData.isCompleted,
+			description: requestData.description,
+		};
+
+		const task = await this.useCaseTask.create(newTask);
+
+		const response = {
+			msg: 'create',
+			...task,
+		}
+
+		return reply.code(200).send(response);
+	}
+
+	private getAllTaskHandler = async (req: FastifyRequest, reply: FastifyReply) => {
+		const tasks = await this.useCaseTask.getAll(req[userId]);
+
+		const response = {
+			msg: 'all',
+			tasks: [...tasks],
+		}
+
+		return reply.code(200).send(response);
+	}
+
+	private getNotCompetedTaskHandler = async (req: FastifyRequest, reply: FastifyReply) => {
+		const tasks = await this.useCaseTask.getNotCompleted(req[userId]);
+
+		const response = {
+			msg: 'notCompeted',
+			tasks: [...tasks],
+		}
+
+		return reply.code(200).send(response);
+	}
+
+	private getCompetedTaskHandler = async (req: FastifyRequest, reply: FastifyReply) => {
+		const tasks = await this.useCaseTask.getCompleted(req[userId]);
+
+		const response = {
+			msg: 'competed',
+			tasks: [...tasks],
+		}
+
+		return reply.code(200).send(response);
+	}
+
 	registerRoutes = async (instance: FastifyInstance, options: FastifyPluginOptions): Promise<void> => {
 		instance.addHook('preHandler', this.authMiddleware.register);
 
-		instance.get('/:uuid', async (request, reply) => {
-
-			const task = await this.useCaseTask.getTask((request.params as {uuid: string})['uuid'], request[userId]);
-
-			const response = {
-				msg: 'get task',
-				...task,
-			}
-			return reply.code(200).send(response);
+		instance.route({
+			method: 'GET',
+			url: '/:uuid',
+			schema: {
+				description: 'get task',
+				tags: ['task'],
+				security: [{ 'apiKey': [] }],
+				response: {
+					...baseHttpResponseMapping
+				} 
+			},
+			handler: this.getTaskHandler
 		})
 
-		instance.patch('/:uuid', async (request, reply) => {
-			const requestData = JSON.parse(request.body as string) as TaskEntity;
-
-			if(!requestData.description && !requestData.title && !requestData.isCompleted) {
-				return reply.code(401).send({ error: 'not valid' })
-			}
-
-			const task = await this.useCaseTask.updateTask(requestData, request[userId]);
-
-			const response = {
-				msg: 'update',
-				...task,
-			}
-
-			return reply.code(200).send(response);
+		instance.route({
+			method: 'PATCH',
+			url: '/',
+			schema: {
+				description: 'patch user',
+				tags: ['task'],
+				security: [{ 'apiKey': [] }],
+				response: {
+					...baseHttpResponseMapping
+				} 
+			},
+			handler: this.patchTaskHandler
 		})
 
-		instance.delete('/:uuid', async (request, reply) => {
-
-			await this.useCaseTask.deleteTask((request.params as {uuid: string})['uuid'], request[userId]);
-
-			return reply.code(200).send({status: 'ok'});
+		instance.route({
+			method: 'DELETE',
+			url: '/',
+			schema: {
+				description: 'delete user',
+				tags: ['task'],
+				security: [{ 'apiKey': [] }],
+				response: {
+					...baseHttpResponseMapping
+				} 
+			},
+			handler: this.deleteTaskHandler
 		})
 
-		instance.post('/create', async (request, reply) => {
-			const requestData = JSON.parse(request.body as string) as TaskEntity;
-
-			if(!requestData.description || !requestData.title || !requestData.isCompleted) {
-				return reply.code(401).send({ error: 'not valid' })
-			}
-
-			const newTask: TaskEntity = {
-				userId: request[userId],
-				title: requestData.title,
-				isCompleted: requestData.isCompleted,
-				description: requestData.description,
-			};
-
-			const task = await this.useCaseTask.create(newTask);
-
-			const response = {
-				msg: 'create',
-				...task,
-			}
-
-			return reply.code(200).send(response);
+		instance.route({
+			method: 'POST',
+			url: '/create/new',
+			schema: {
+				description: 'create task',
+				tags: ['task'],
+				security: [{ 'apiKey': [] }],
+				response: {
+					...baseHttpResponseMapping
+				} 
+			},
+			handler: this.createTaskHandler
 		})
 
-		instance.get('/all', async (request, reply) => {
-
-			const tasks = await this.useCaseTask.getAll(request[userId]);
-
-			const response = {
-				msg: 'all',
-				tasks: [...tasks],
-			}
-
-			return reply.code(200).send(response);
+		instance.route({
+			method: 'GET',
+			url: '/get/all',
+			schema: {
+				description: 'get all task',
+				tags: ['task'],
+				security: [{ 'apiKey': [] }],
+				response: {
+					...baseHttpResponseMapping
+				} 
+			},
+			handler: this.getAllTaskHandler
 		})
 
-		instance.get('/notCompeted', async (request, reply) => {
-
-			const tasks = await this.useCaseTask.getNotCompleted(request[userId]);
-
-			const response = {
-				msg: 'notCompeted',
-				tasks: [...tasks],
-			}
-
-			return reply.code(200).send(response);
+		instance.route({
+			method: 'GET',
+			url: '/get/notCompeted',
+			schema: {
+				description: 'get all not compete task',
+				tags: ['task'],
+				security: [{ 'apiKey': [] }],
+				response: {
+					...baseHttpResponseMapping
+				} 
+			},
+			handler: this.getNotCompetedTaskHandler
 		})
 
-		instance.get('/competed', async (request, reply) => {
-
-			const tasks = await this.useCaseTask.getCompleted(request[userId]);
-
-			const response = {
-				msg: 'competed',
-				tasks: [...tasks],
-			}
-
-			return reply.code(200).send(response);
+		instance.route({
+			method: 'GET',
+			url: '/get/competed',
+			schema: {
+				description: 'get all compete task',
+				tags: ['task'],
+				security: [{ 'apiKey': [] }],
+				response: {
+					...baseHttpResponseMapping
+				} 
+			},
+			handler: this.getCompetedTaskHandler
 		})
 	}
 }
