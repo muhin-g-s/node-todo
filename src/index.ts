a97e50d4c12ed0abe6c812b235ad15741bfbabcd
Мухин Григорий <muhin.g.s.00@gmail.com>
2024-10-26 13:35:07 +0300

    refactoring repository

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { createResponseErrorInternal, createResponseBadRequest, createResponseNotFound } from './controllers/http/response/error';
 import "reflect-metadata"
 
-import { TaskHandler,prefixTask } from './controllers/http/v1/task';
-import {prefixAuth, AuthHandler} from './controllers/http/v1/auth';
+import { TaskHandler, prefixTask } from './controllers/http/v1/task';
+import { prefixAuth, AuthHandler } from './controllers/http/v1/auth';
 import { AuthMiddleware } from './controllers/http/middleware/auth';
 import { prefixUser, UserHandler } from './controllers/http/v1/user';
 import { UseCaseTask } from './domain/use-cases/task';
@@ -26,74 +26,74 @@ const globalPrefix = 'api/v1' as const;
 try {
 	(async () => {
 
-			const db = await sqliteClient.initialize();
-			const userRepository = new UserRepository(db.manager);
-			const taskRepository = new TaskRepository(db.manager);
+		const db = await sqliteClient.initialize();
+		const userRepository = new UserRepository(db.manager);
+		const taskRepository = new TaskRepository(db.manager);
 
-			const authManager = new AuthManager();
+		const authManager = new AuthManager();
 
-			const userService = new UserService(userRepository);
-			const taskService = new TaskService(taskRepository);
-			const authService = new AuthService(userRepository, authManager);
+		const userService = new UserService(userRepository);
+		const taskService = new TaskService(taskRepository);
+		const authService = new AuthService(userRepository, authManager);
 
-			const useCaseAuth = new UseCaseAuth(authService, userService);
-			const useCaseUser = new UseCaseUser(userService);
-			const useCaseTask = new UseCaseTask(taskService);
+		const useCaseAuth = new UseCaseAuth(authService, userService);
+		const useCaseUser = new UseCaseUser(userService);
+		const useCaseTask = new UseCaseTask(taskService);
 
-			const authMiddleware =  new AuthMiddleware(authManager);
-			const authHandler =     new AuthHandler(useCaseAuth);
-			const userHandler =     new UserHandler(useCaseUser, authMiddleware);
-			const taskHandler =     new TaskHandler(useCaseTask, authMiddleware);
+		const authMiddleware = new AuthMiddleware(authManager);
+		const authHandler = new AuthHandler(useCaseAuth);
+		const userHandler = new UserHandler(useCaseUser, authMiddleware);
+		const taskHandler = new TaskHandler(useCaseTask, authMiddleware);
 
-			const server: FastifyInstance = Fastify();
-			authMiddleware.addRequestUserId(server);
-			
-			server.setValidatorCompiler(validatorCompiler);
-			server.setSerializerCompiler(serializerCompiler);
+		const server: FastifyInstance = Fastify();
+		authMiddleware.addRequestUserId(server);
 
-			server.setErrorHandler((err, req, reply) => {
-				if (hasZodFastifySchemaValidationErrors(err)) {
-						return createResponseBadRequest(req, reply);
-				}
-		
-				if (isResponseSerializationError(err)) {
-						return createResponseErrorInternal(req, reply);
-				}
-			})
+		server.setValidatorCompiler(validatorCompiler);
+		server.setSerializerCompiler(serializerCompiler);
 
-			server.setNotFoundHandler(createResponseNotFound);
+		server.setErrorHandler((err, req, reply) => {
+			if (hasZodFastifySchemaValidationErrors(err)) {
+				return createResponseBadRequest(req, reply);
+			}
 
-			server.register(fastifySwagger, {
-				openapi: {
-					info: {
-						title: 'node todo',
-						version: '1.0.0',
-					},
-					servers: [],
-					components: {
-						securitySchemes: {
-							apiKey: {
-								type: 'apiKey',
-								name: 'authorization',
-								in: 'header'
-							}
-						}
-					},
+			if (isResponseSerializationError(err)) {
+				return createResponseErrorInternal(req, reply);
+			}
+		})
+
+		server.setNotFoundHandler(createResponseNotFound);
+
+		server.register(fastifySwagger, {
+			openapi: {
+				info: {
+					title: 'node todo',
+					version: '1.0.0',
 				},
-				transform: jsonSchemaTransform,
-			
-			});
+				servers: [],
+				components: {
+					securitySchemes: {
+						apiKey: {
+							type: 'apiKey',
+							name: 'authorization',
+							in: 'header'
+						}
+					}
+				},
+			},
+			transform: jsonSchemaTransform,
 
-			server.register(fastifySwaggerUi, {
-				routePrefix: '/doc',
-			});
+		});
 
-			await server.register(authHandler.registerRoutes, { prefix: globalPrefix + prefixAuth });
-			await server.register(userHandler.registerRoutes, {prefix: globalPrefix + prefixUser });
-			await server.register(taskHandler.registerRoutes, {prefix: globalPrefix + prefixTask });
+		server.register(fastifySwaggerUi, {
+			routePrefix: '/doc',
+		});
 
-			const port = await server.listen({ port: 8080 });
-			console.log(`Server at 8080 ${port}/doc`);
+		await server.register(authHandler.registerRoutes, { prefix: globalPrefix + prefixAuth });
+		await server.register(userHandler.registerRoutes, { prefix: globalPrefix + prefixUser });
+		await server.register(taskHandler.registerRoutes, { prefix: globalPrefix + prefixTask });
+
+		const port = await server.listen({ port: 8080 });
+		console.log(`Server at 8080 ${port}/doc`);
 	})();
 } catch (err) {
 	console.error(err);
