602fcc83b3e76bd90fe59133d2d0f6a518db205b
Мухин Григорий <muhin.g.s.00@gmail.com>
2024-10-27 18:35:50 +0300

    refactoring and add catching errors through either design in task service

--- src/domain/services/task/index.ts
+++ src/domain/services/task/index.ts
@@ -1,81 +1,123 @@
 import { CreateTask, FindTask, Task, UpdateTask } from '@/domain/entities/task';
+import { Either, ErrorResult, Result } from '@/lib';
+
+const enum RepositoryError {
+	UnknownError,
+	NotFoundTask,
+}
+
+const enum ServiceError {
+	UnknownError,
+	NotFoundTask,
+	NotBelongingUser,
+}
 
 interface ITaskRepository {
-	findManyByUserId(userId: string): Promise<Task[]>;
-	save(task: CreateTask): Promise<Task>;
-	findById(taskId: string): Promise<Task>;
-	update(task: UpdateTask): Promise<Task>;
-	delete(taskId: string): Promise<void>;
+	findManyByUserId(userId: string): Promise<Either<RepositoryError, Task[]>>;
+	save(task: CreateTask): Promise<Either<RepositoryError, Task>>;
+	findById(taskId: string): Promise<Either<RepositoryError, Task>>;
+	update(task: UpdateTask): Promise<Either<RepositoryError, Task>>;
+	delete(taskId: string): Promise<Either<RepositoryError, void>>;
 }
 
 export class TaskService {
 	constructor(private taskRepository: ITaskRepository) { }
 
-	create(task: CreateTask): Promise<Task> {
-		return this.taskRepository.save(task);
+	async create(task: CreateTask): Promise<Either<ServiceError, Task>> {
+		const createdTaskResult = await this.taskRepository.save(task);
+
+		return createdTaskResult.isError()
+			? ErrorResult.create(ServiceError.UnknownError)
+			: Result.create(createdTaskResult.value);
 	}
 
-	async update(task: UpdateTask): Promise<Task> {
-		const existedTask = await this.getById(task);
+	async update(task: UpdateTask): Promise<Either<ServiceError, Task>> {
+		const foundTaskResult = await this.getById(task);
 
-		if (this.checkBelongingTaskToUser(existedTask.userId, task.userId)) {
-			throw new Error('task not belonging user');
+		if (foundTaskResult.isError()) {
+			return ErrorResult.create(foundTaskResult.error);
 		}
 
+		const foundTask = foundTaskResult.value;
+
 		if (task.description) {
-			existedTask.description = task.description;
+			foundTask.description = task.description;
 		}
 
 		if (task.isCompleted) {
-			existedTask.isCompleted = task.isCompleted;
+			foundTask.isCompleted = task.isCompleted;
 		}
 
 		if (task.title) {
-			existedTask.title = task.title;
+			foundTask.title = task.title;
 		}
 
-		return this.taskRepository.update(existedTask);
+		const updateTaskResult = await this.taskRepository.update(foundTask);
+
+		return updateTaskResult.isError()
+			? ErrorResult.create(ServiceError.UnknownError)
+			: Result.create(updateTaskResult.value);
 	}
 
-	async getById({ id, userId }: FindTask): Promise<Task> {
-		const existedTask = await this.taskRepository.findById(id);
+	async getById({ id, userId }: FindTask): Promise<Either<ServiceError, Task>> {
+		const foundTaskResult = await this.taskRepository.findById(id);
 
-		if (this.checkBelongingTaskToUser(existedTask.userId, userId)) {
-			throw new Error('task not belonging user');
+		if (foundTaskResult.isError()) {
+			const { error } = foundTaskResult;
+
+			switch (error) {
+				case RepositoryError.UnknownError: return ErrorResult.create(ServiceError.UnknownError);
+				case RepositoryError.NotFoundTask: return ErrorResult.create(ServiceError.NotFoundTask);
+				default: return ErrorResult.create(ServiceError.UnknownError);
+			}
 		}
 
-		return existedTask;
+		const foundTask = foundTaskResult.value;
+
+		return this.checkBelongingTaskToUser(foundTask.userId, userId)
+			? Result.create(foundTask)
+			: ErrorResult.create(ServiceError.NotBelongingUser);
 	}
 
-	getAll(userId: string): Promise<Task[]> {
-		return this.taskRepository.findManyByUserId(userId);
+	async getAll(userId: string): Promise<Either<ServiceError, Task[]>> {
+		const foundTasksResult = await this.taskRepository.findManyByUserId(userId);
+
+		return foundTasksResult.isError()
+			? ErrorResult.create(ServiceError.UnknownError)
+			: Result.create(foundTasksResult.value);
 	}
 
-	async getNotCompleted(userId: string): Promise<Task[]> {
-		const tasks = await this.taskRepository.findManyByUserId(userId);
+	async getNotCompleted(userId: string): Promise<Either<ServiceError, Task[]>> {
+		const foundTasksResult = await this.taskRepository.findManyByUserId(userId);
 
-		return tasks.filter(el => !el.isCompleted);
+		return foundTasksResult.isError()
+			? ErrorResult.create(ServiceError.UnknownError)
+			: Result.create(foundTasksResult.value.filter(el => !el.isCompleted));
 	}
 
-	async getCompleted(userId: string): Promise<Task[]> {
-		const tasks = await this.taskRepository.findManyByUserId(userId);
+	async getCompleted(userId: string): Promise<Either<ServiceError, Task[]>> {
+		const foundTasksResult = await this.taskRepository.findManyByUserId(userId);
 
-		return tasks.filter(el => el.isCompleted);
+		return foundTasksResult.isError()
+			? ErrorResult.create(ServiceError.UnknownError)
+			: Result.create(foundTasksResult.value.filter(el => el.isCompleted));
 	}
 
-	async delete({ id, userId }: FindTask): Promise<Task> {
-		const existedTask = await this.taskRepository.findById(id);
+	async delete(task: FindTask): Promise<Either<ServiceError, Task>> {
+		const foundTaskResult = await this.getById(task);
 
-		if (this.checkBelongingTaskToUser(existedTask.userId, userId)) {
-			throw new Error('task not belonging user');
+		if (foundTaskResult.isError()) {
+			return ErrorResult.create(foundTaskResult.error);
 		}
 
-		await this.taskRepository.delete(id);
+		const deleteTaskResult = await this.taskRepository.delete(task.id);
 
-		return existedTask;
+		return deleteTaskResult.isError()
+			? ErrorResult.create(ServiceError.UnknownError)
+			: Result.create(foundTaskResult.value)
 	}
 
-	private checkBelongingTaskToUser(taskId: string, userId: string): boolean {
-		return taskId === userId;
+	private checkBelongingTaskToUser(supposedUserId: string, userId: string): boolean {
+		return supposedUserId === userId;
 	}
 }
